29014196fde371279095efc04480a1c713a81ac0
unknown <amrvijayhiremath@gmail.com>
2022-03-29 17:45:20 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { Todo } from 'src/models/todo.model';
+import { Guid } from 'guid-typescript'
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDo-App';
+  todos: Todo[] = [
+    new Todo(Guid.create(), 'wash Car',false),
+    new Todo(Guid.create(), 'buy Groceries',false),
+  ]
+
+
+  onsubmit(form : NgForm){
+    let todo = new Todo(Guid.create(),form.value.title, false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+
+  onComplete(id: Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    let index = this.todos.indexOf(todo,0);
+    if(index > -1){
+      this.todos.splice(index,1);
+    }
+  }
 }
